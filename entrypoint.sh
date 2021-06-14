3adb557763e8c85ba1c5d0f792f6abb7b93f20db
Substicious <68825499+substicious@users.noreply.github.com>
2021-06-14 13:39:44 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Make sure we're in the correct dir
+cd /home/container/
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container/$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
